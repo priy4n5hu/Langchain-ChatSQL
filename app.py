f89fcb99eacd5904d274d08060417f14d465db8a
Priyanshu <priyanshujassal68@gmail.com>
2026-06-19 16:56:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,94 @@
+import streamlit as st
+from pathlib import Path
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_community.utilities import SQLDatabase
+from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from sqlalchemy import create_engine
+import sqlite3
+from langchain_groq import ChatGroq
+
+st.set_page_config(page_title="Langchain: Chat with SQL DB", page_icon="🦜")
+st.title("🦜 Langchain: chat with SQL DB")
+
+LOCALDB = "USE_LOCALDB"
+MYSQL = "USE_MYSQL"
+
+radio_opt = ["Use SQLite3 database - Student.db", "connect to your SQL Database"]
+selected_opt = st.sidebar.radio(label="Choose your DB", options=radio_opt)
+
+if radio_opt.index(selected_opt) == 1:
+    db_uri = MYSQL
+    mysql_host = st.sidebar.text_input("Provide My SQL Host")
+    mysql_user = st.sidebar.text_input("MYSQL User")
+    mysql_password = st.sidebar.text_input("MYSQL Password", type="password")
+    mysql_db = st.sidebar.text_input("MySQL database")
+else:
+    db_uri = LOCALDB
+    mysql_host = mysql_user = mysql_password = mysql_db = None
+
+api_key = st.sidebar.text_input(label="GROQ API KEY", type="password")
+
+if not db_uri:
+    st.info("Please enter the database information and uri")
+    st.stop()
+
+if not api_key:
+    st.info("Please add the GROQ API key")
+    st.stop()
+
+# LLM model
+llm = ChatGroq(
+    groq_api_key=api_key,
+    model_name="llama-3.3-70b-versatile",
+    temperature=0
+)
+
+
+@st.cache_resource(ttl="2h")
+def configure_db(db_uri, mysql_host=None, mysql_user=None, mysql_password=None, mysql_db=None):
+    if db_uri == LOCALDB:
+        db_filepath = (Path(__file__).parent / "student.db").absolute()
+        print(db_filepath)
+        creator = lambda: sqlite3.connect(f"file:{db_filepath}?mode=ro", uri=True)
+        return SQLDatabase(create_engine("sqlite:///", creator=creator))
+    elif db_uri == MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("Please provide all MySQL connection details.")
+            st.stop()
+        return SQLDatabase(create_engine(
+            f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"
+        ))
+
+
+if db_uri == MYSQL:
+    db = configure_db(db_uri, mysql_host, mysql_user, mysql_password, mysql_db)
+else:
+    db = configure_db(db_uri)
+
+## toolkit
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+agent = create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+user_query = st.chat_input(placeholder="Ask anything from the database")
+
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(user_query, callbacks=[streamlit_callback])
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response)
